3a79de6390286fb9ed82a12d05dbc7fe0830263c
Yeah Right <chekio+htmlcssjvscrpt@users.noreply.github.com>
2023-07-21 07:56:25 +0800

    Add 404.html

--- hello.py
+++ hello.py
@@ -27,3 +27,15 @@ def index():
 def user(name):
     return render_template("user.html", user_name=name)
 
+# Create Custom Error Pags
+
+# Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+# Internal Server Error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
+
